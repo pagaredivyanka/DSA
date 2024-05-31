4a8bf7904bfb6742fcdf6a7986ed8276fb7ca6d3
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-05-31 23:30:40 +0530

    Type casting

---
+++ first_idea_program/src/com/divyanka/TypeCasting.java
@@ -0,0 +1,11 @@
+package com.divyanka;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = (int) (90.68f);
+        System.out.println(num);
+    }
+}
