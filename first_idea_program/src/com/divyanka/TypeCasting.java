35a8fcb64b029d1588a5d5c44cf985c4e84b8efb
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-06-02 20:03:18 +0530

    Update TypeCasting.java

--- first_idea_program/src/com/divyanka/TypeCasting.java
+++ first_idea_program/src/com/divyanka/TypeCasting.java
@@ -7,5 +7,21 @@ public class TypeCasting {
         Scanner input = new Scanner(System.in);
         int num = (int) (90.68f);
         System.out.println(num);
+
+        
+        //type casting
+//        int num = (int)(88.56f);
+//        System.out.println(num);
+
+        //automatically type promotion in expressions
+//        int a = 257;
+//        byte b = (byte)(a); // 257 % 256 = 1
+//        System.out.println(b);
+
+        byte a = 40;
+        byte b = 60;
+        byte c = 100;
+        int d = a* b / c;
+        System.out.println(d);
     }
 }
