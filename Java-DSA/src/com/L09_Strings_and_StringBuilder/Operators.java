afca4beb1897efba5198a612d71f6e8ea5dcbde7
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-10-15 23:38:13 +0530

    Add files via upload

---
+++ Java-DSA/src/com/L09_Strings_and_StringBuilder/Operators.java
@@ -0,0 +1,24 @@
+package com.L09_Strings_and_StringBuilder;
+
+import java.util.ArrayList;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // 195
+        System.out.println("a" + "b"); // ab
+        System.out.println((char) ('a' + 3)); // d
+
+
+        // vvimp --> when integer is concatenated with a string then it convrted to it's wrapper class Integer
+        System.out.println("a" + 1); // a1
+        // integer will be converted to Integer that will call toString()
+        // this is same as after a few steps: "a" + "1" --> a1
+
+        System.out.println("Divu" + new ArrayList<>()); // Divu[]
+        System.out.println("Divu" + new Integer(56)); // Divu56
+        String ans = new Integer(56) + "" + new ArrayList<>(); // we can do it with any complex object but for that any of the object must be of type String
+        System.out.println(ans); // 56[]
+
+
+    }
+}
---
+++ Java-DSA/src/com/L09_Strings_and_StringBuilder/PrettyPrinting.java
@@ -0,0 +1,15 @@
+package com.L09_Strings_and_StringBuilder;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 354.54545f;
+        System.out.printf("formatted number is %.2f", a);  // 354.55
+        System.out.println("\n");
+
+        System.out.printf("Pie = %.3f", Math.PI);
+        System.out.println("\n");
+
+        System.out.printf("Hello my name is %s and I am a %s", "Divu", "Girl");
+        System.out.println("\n");
+    }
+}
