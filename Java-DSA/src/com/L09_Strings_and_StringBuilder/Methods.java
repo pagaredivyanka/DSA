9d262b98431db96618ef5542875e800c0ae164bd
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-10-16 00:33:52 +0530

    Add files via upload

---
+++ Java-DSA/src/com/L09_Strings_and_StringBuilder/Methods.java
@@ -0,0 +1,16 @@
+package com.L09_Strings_and_StringBuilder;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Divyanka Pagare Hello World";
+
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name);
+        System.out.println(name.indexOf('a'));
+        System.out.println("       Divyanka       ".strip());
+        System.out.println(Arrays.toString(name.split(" ")));
+    }
+}
